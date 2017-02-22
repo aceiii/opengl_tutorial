1bd09e1a3454618893ed09b3e45b217ceb125ae2
Borin Ouch <borin.ouch@gmail.com>
2017-02-21 16:49:33 -0800

    Initial commit

---
+++ build.sh
@@ -0,0 +1,2 @@
+
+g++ main.cpp -I/usr/local/include -L/usr/lib -L/usr/local/lib -lglfw3 -framework OpenGL -o glfw3-test
---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+void render() {
+    glClearColor(0.787, 0.944, 1.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+}
+
+int main() {
+
+    if (!glfwInit()) {
+        std::cerr << "GLFW: Init error." << std::endl;
+    }
+
+    int major, minor, revision;
+    glfwGetVersion(&major, &minor, &revision);
+    std::cout << "GLFW: Version: " << major << "." << minor << "." << revision << std::endl;
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Hello, world!", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "GLFW: CreateWindow error!" << std::endl;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (true) {
+        if (glfwWindowShouldClose(window) == GLFW_TRUE) {
+            std::cout << "GLFW: Closing window." << std::endl;
+            break;
+        }
+
+        render();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    if (window) {
+        glfwDestroyWindow(window);
+        window = nullptr;
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
+
+
