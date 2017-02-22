18c2a4211bf2b65ed307b7337b3a2ab9aae8f886
Borin Ouch <borin.ouch@gmail.com>
2017-02-21 18:06:31 -0800

    Draw triangle

---
+++ default.vsh
@@ -0,0 +1,9 @@
+
+
+attribute vec4 position;
+
+void main()
+{
+    gl_Position = vec4(position.x, position.y, position.z, 1.0);
+}
+
--- main.cpp
+++ main.cpp
@@ -1,9 +1,104 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <GLFW/glfw3.h>
+
+namespace {
+    GLFWwindow *window = nullptr;
+
+    GLfloat vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+    GLuint vboHandle;
+    GLuint shaderProgram;
+}
+
+GLuint loadShaderFile(GLenum shaderType, const std::string &filename) {
+    std::ifstream f(filename);
+    if (!f.is_open()) {
+        std::cerr << "Unable to open file: " << filename << std::endl;
+        return 0;
+    }
+
+    std::string sourceFile((std::istreambuf_iterator<char>(f)), std::istreambuf_iterator<char>());
+    const GLchar *shaderSource[] = { sourceFile.c_str() };
+
+    GLuint handle = glCreateShader(shaderType);
+    glShaderSource(handle, 1, shaderSource, nullptr);
+    glCompileShader(handle);
+
+    GLint success;
+    glGetShaderiv(handle, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        GLchar infoLog[512];
+        glGetShaderInfoLog(handle, 512, NULL, infoLog);
+        std::cerr << "OpenGL: Shader compilation failed: " << infoLog << std::endl;
+        return 0;
+    }
+
+    return handle;
+}
+
+GLuint createShaderProgram(GLuint vertexShaderHandle, GLuint fragmentShaderHandle) {
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShaderHandle);
+    glAttachShader(shaderProgram, fragmentShaderHandle);
+    glLinkProgram(shaderProgram);
+
+    GLint success;
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        GLchar infoLog[512];
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cerr << "OpenGL: Shader program link failed: " << infoLog << std::endl;
+        return 0;
+    }
+
+    return shaderProgram;
+}
+
+bool setupOpengl() {
+    int w, h;
+    glfwGetFramebufferSize(window, &w, &h);
+
+    glViewport(0, 0, w, h);
+
+    glGenBuffers(1, &vboHandle);
+
+    GLuint vertexShaderHandle = loadShaderFile(GL_VERTEX_SHADER, "default.vsh");
+    if (!vertexShaderHandle) {
+        std::cerr << "Failed to create vertex shader." << std::endl;
+        return false;
+    }
+
+    GLuint fragmentShaderHandle = loadShaderFile(GL_FRAGMENT_SHADER, "default.fsh");
+    if (!fragmentShaderHandle) {
+        std::cerr << "Failed to create fragment shader." << std::endl;
+        return false;
+    }
+
+    shaderProgram = createShaderProgram(vertexShaderHandle, fragmentShaderHandle);
+    if (!shaderProgram) {
+        std::cerr << "Failed to create shader program." << std::endl;
+    }
+
+    return true;
+}
 
 void render() {
     glClearColor(0.787, 0.944, 1.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vboHandle);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    glUseProgram(shaderProgram);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
 int main() {
@@ -19,7 +114,7 @@ int main() {
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
     int width = 800, height = 600;
-    GLFWwindow *window = glfwCreateWindow(width, height, "Hello, world!", nullptr, nullptr);
+    window = glfwCreateWindow(width, height, "Hello, world!", nullptr, nullptr);
     if (!window) {
         std::cerr << "GLFW: CreateWindow error!" << std::endl;
 
@@ -36,7 +131,13 @@ int main() {
     glfwGetFramebufferSize(window, &frameBufferWidth, &frameBufferHeight);
     std::cout << "GLFW: Framebuffer size: (" << frameBufferWidth << ", " << frameBufferHeight << ")" << std::endl;
 
-    glViewport(0, 0, 200, 200);
+    if (!setupOpengl()) {
+        std::cerr << "OpenGL: Setup error!" << std::endl;
+
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return -1;
+    }
 
     while (true) {
         if (glfwWindowShouldClose(window) == GLFW_TRUE) {
