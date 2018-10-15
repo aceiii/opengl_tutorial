205bc60e72f2113353005addece65d2b9c0962c9
Borin Ouch <borin.ouch@gmail.com>
2018-10-14 19:36:17 -0700

    Use c++14

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ SOURCE_FILES="main.cpp \
     imgui_draw.cpp \
     imgui_impl_glfw_gl3.cpp"
 
-g++ $SOURCE_FILES -I/usr/local/include -L/usr/lib -L/usr/local/lib -lglfw3 -lsoil -framework CoreFoundation -framework OpenGL -o build/glfw3-test
+g++ -std=c++14 $SOURCE_FILES -I/usr/local/include -L/usr/lib -L/usr/local/lib -lglfw3 -lsoil -framework CoreFoundation -framework OpenGL -o build/glfw3-test
