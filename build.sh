ad695801e144039b9eccf3b4aa7cfcb430ded2bd
Borin Ouch <borin.ouch@gmail.com>
2018-10-14 19:51:48 -0700

    Use single almagamted build build

--- build.sh
+++ build.sh
@@ -2,11 +2,6 @@
 
 mkdir -p build
 
-SOURCE_FILES="main.cpp \
-    shader.cpp \
-    imgui.cpp \
-    imgui_demo.cpp \
-    imgui_draw.cpp \
-    imgui_impl_glfw_gl3.cpp"
+SOURCE_FILES="build.cpp"
 
 g++ -std=c++14 $SOURCE_FILES -I/usr/local/include -L/usr/lib -L/usr/local/lib -lglfw3 -lsoil -framework CoreFoundation -framework OpenGL -o build/glfw3-test
